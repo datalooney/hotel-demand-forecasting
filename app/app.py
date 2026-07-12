02d2733f03c8c97803a444859d06811361669140
datalooney <plp2025shraddha@gmail.com>
2026-07-12 17:49:31 +0000

    streamlit app to generate forecast

---
+++ app/app.py
@@ -0,0 +1,63 @@
+import streamlit as st
+from predfunc import forecast_bookings
+st.set_page_config(page_title="Hotel Demand Forecasting",
+                   layout= "wide")
+
+st.title("Hotel Demand Forecating")
+st.write( "Forecast hotel room bookings using trained XGBoost models.")
+
+#---------------------#
+#  SideBar            #
+#---------------------#
+st.sidebar.header("Forecast Settings")
+start_date = st.sidebar.date_input("Forecast Start Date",value=None)
+end_date = st.sidebar.date_input("Forecast End Date",value=None)
+
+#--------------------#
+#       Predict      #
+# -------------------#  
+predict = st.sidebar.button("Generate Forecast")
+
+#--------------------#
+#   Call Function    #
+#--------------------#
+if predict:
+
+    with st.spinner("Generating forecasts..."):
+
+        forecast_df = forecast_bookings(
+            data_path= "/workspaces/hotel-demand-forecasting/data/hotel_daily_booking_data_2024_2025.xlsx",
+            model1_path="/workspaces/hotel-demand-forecasting/models/hotel_demand_xgb.pkl",
+            model2_path="/workspaces/hotel-demand-forecasting/models/hotel_demand_xgb_price.pkl",
+            encoder_path="/workspaces/hotel-demand-forecasting/models/label_encoders.pkl",
+            feature_path="/workspaces/hotel-demand-forecasting/models/features.pkl",
+            start_date=start_date,
+            end_date=end_date
+        )
+
+    st.success("Forecast Complete!")
+
+    #---------------------#
+    #  Display Forecasts  #
+    #---------------------#
+    st.subheader("Forecast Results")
+    st.dataframe(forecast_df, use_container_width = True)
+
+    #-------------------------#
+    #   Add room selector     
+    #-------------------------#
+    room = st.selectbox(  "Room Type",
+    forecast_df["room_type"].unique()
+    )
+
+    room_df = forecast_df[ forecast_df["room_type"] == room]
+
+    #---------------------------#
+    #         Plots             #
+    #---------------------------#
+    import plotly.express as px
+
+    fig = px.line(room_df,   x="date",   y="forecast",     title=f"{room} Forecast")
+
+    st.plotly_chart( fig,   use_container_width=True
+)
